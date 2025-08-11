33f823c7a50ffb10da270b058a798e5a4c238928
Harel Yacovian <harelyac@gmail.com>
2025-08-11 16:33:53 +0300

    Update providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,13 +1,16 @@
 terraform {
   required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.116"
     }
   }
-  # Optional: remote state in Azure Storage
-  # backend "azurerm" {}
+
+  # backend "azurerm" {}  # (optional) if you use remote state
 }
 
-provider "azurerm" { features {} }
+provider "azurerm" {
+  features {}
+}
