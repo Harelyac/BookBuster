484e94ebfb8a517b57d2730c813755de49d4b6a6
Harel Yacovian <harelyac@gmail.com>
2025-08-11 17:15:09 +0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,23 @@
-# Existing Resource Group
+# Use existing Resource Group
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-# Existing App Service Plan
-data "azurerm_service_plan" "plan" {
+# Create App Service Plan
+resource "azurerm_service_plan" "plan" {
   name                = var.app_service_plan_name
+  location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1" # Adjust to your needs (S1, P1v3, etc.)
 }
 
-# Create the Linux Web App, using existing RG & Plan
+# Create the Linux Web App
 resource "azurerm_linux_web_app" "app" {
   name                = var.webapp_name
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-  service_plan_id     = data.azurerm_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
     application_stack {
